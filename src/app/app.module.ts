fdd98d3e1aeb757aa28d9ad2dc7550643e857c22
Calvin <cmogodi@gmail.com>
2018-10-02 21:48:59 +0200

    dww

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProgrammeComponent } from './programme/programme.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { PackageComponent } from './package/package.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProgrammeComponent,
+    SpeakersComponent,
+    PackageComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule
