4f92bfef5101273866a45af6f7ac479dd11339a0
Calvin <cmogodi@gmail.com>
2018-10-05 22:55:24 +0200

    most recent UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public timer = 0;
+  public days = 0;
+  public hours = 0;
+  public minutes = 0;
+  public seconds = 0;
 
   constructor() { }
 
   ngOnInit() {
-    
+    this.timerTick();
+  }
+
+  timerTick() {
+    setTimeout(() => {
+      let toDate = new Date("2018/11/20 8:00 AM");
+      //let toDate = new Date("2018/10/0 15:4 PM");
+      var now = new Date();
+      var difference = toDate.getTime() - now.getTime();
+      if (difference <= 0 || isNaN(difference)) { return; }
+      var seconds = Math.floor(difference / 1000);
+      var minutes = Math.floor(seconds / 60);
+      var hours = Math.floor(minutes / 60);
+      var days = Math.floor(hours / 24);
+  
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+  
+      this.days = days;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+      if (difference > 0) {
+        this.timerTick();
+      }
+    }, 1000);
   }
 
 }
