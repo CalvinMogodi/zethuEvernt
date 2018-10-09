5071d7cfe000db57c89b885fcd4ebf3dccf14433
Calvin <cmogodi@gmail.com>
2018-10-09 16:05:02 +0200

    register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
-
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'zethuevernt';
@@ -14,27 +33,27 @@ export class AppComponent {
   public verificationCode = undefined;
   itemValue = 'worrking';
   public showError = false;
-
+  public isOpen = false;
+  public showSuc = false;
   element: HTMLElement;
   constructor(public db: AngularFireDatabase) {
     this.verificationCode = undefined;
   }
 
-  onSubmit() {
-    if(this.verificationCode != undefined){
+  procced() {
+    if (this.verificationCode != undefined) {
       let code = this.verificationCode.toLowerCase().trim();
+      this.isOpen = true;
       this.db.database.ref('/guests/' + code).once('value').then(snapshot => {
         var user = snapshot.val();
-        if(user != null){
-          this.db.list('/guests/confirmed').push({ content: true });
-          this.element = document.getElementById('closeModel') as HTMLElement;
-          this.element.click();      
-          this.verificationCode = undefined;
-          this.showError = false;
-        }else{
-          this.showError = true;
+        if (user != null) {
         }
-    });    
+      })
     }
+  }
+  onSubmit() {
+    this.db.list('/guests/confirmed').push({ content: true });
+    this.showSuc = true;
+    this.showError = false;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { environment } from '../environments/environment';
 import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { FormsModule , ReactiveFormsModule} from '@angular/forms';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule, // for database
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
